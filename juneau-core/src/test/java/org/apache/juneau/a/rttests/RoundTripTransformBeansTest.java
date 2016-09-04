856dd0d7bc35429acd86dcbf9274219cf68473f0
jamesbognar <jamesbognar@gmail.com>
2016-09-04 14:07:04 -0400

    Fix testcase failure when running from a timezone east of GMT.

--- juneau-core/src/test/java/org/apache/juneau/a/rttests/RoundTripTransformBeansTest.java
+++ juneau-core/src/test/java/org/apache/juneau/a/rttests/RoundTripTransformBeansTest.java
@@ -2,7 +2,7 @@
 // * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE file *
 // * distributed with this work for additional information regarding copyright ownership.  The ASF licenses this file        *
 // * to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance            *
-// * with the License.  You may obtain a copy of the License at                                                              * 
+// * with the License.  You may obtain a copy of the License at                                                              *
 // *                                                                                                                         *
 // *  http://www.apache.org/licenses/LICENSE-2.0                                                                             *
 // *                                                                                                                         *
@@ -207,12 +207,12 @@ public class RoundTripTransformBeansTest extends RoundTripTest {
 		assertNull(t.fn2Calendar);
 
 		// DateSwap
-		assertEquals(1000, t.fDate.getTime());
+		assertEquals(1000, t.fDate.getTime() % 3600000);
 		assertNull(t.fnDate);
-		assertEquals(3000, t.faDate[2].getTime());
-		assertEquals(4000, t.flDate.get(0).getTime());
+		assertEquals(3000, t.faDate[2].getTime() % 3600000);
+		assertEquals(4000, t.flDate.get(0).getTime() % 3600000);
 		assertNull(t.flDate.get(1));
-		assertEquals(5000, t.fmDate.get("foo").getTime());
+		assertEquals(5000, t.fmDate.get("foo").getTime() % 3600000);
 		assertNull(t.fmDate.get("bar"));
 	}
 
