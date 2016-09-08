b9a829ba6ba01499f1c4fc4f6818b77fde7b1334
jamesbognar <jamesbognar@gmail.com>
2016-09-08 15:33:52 -0400

    Fix up cleanup rules and remove extra spaces from copyright messages.

--- juneau-core/src/main/java/org/apache/juneau/msgpack/MsgPackInputStream.java
+++ juneau-core/src/main/java/org/apache/juneau/msgpack/MsgPackInputStream.java
@@ -2,7 +2,7 @@
 // * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE file *
 // * distributed with this work for additional information regarding copyright ownership.  The ASF licenses this file        *
 // * to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance            *
-// * with the License.  You may obtain a copy of the License at                                                              * 
+// * with the License.  You may obtain a copy of the License at                                                              *
 // *                                                                                                                         *
 // *  http://www.apache.org/licenses/LICENSE-2.0                                                                             *
 // *                                                                                                                         *
@@ -396,7 +396,7 @@ public final class MsgPackInputStream extends InputStream {
 	String readString() throws IOException {
 		return new String(readBinary(), IOUtils.UTF8);
 	}
-	
+
 	/**
 	 * Read a binary field from the stream.
 	 */
