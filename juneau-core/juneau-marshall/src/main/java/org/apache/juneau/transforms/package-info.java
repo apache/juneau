c4fdbf9bb3dcb95c26977887953fda37cc94e9ab
JamesBognar <jamesbognar@apache.org>
2018-02-01 20:40:03 -0500

    Update Javadocs.

--- juneau-core/juneau-marshall/src/main/java/org/apache/juneau/transforms/package-info.java
+++ juneau-core/juneau-marshall/src/main/java/org/apache/juneau/transforms/package-info.java
@@ -10,30 +10,9 @@
 // * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the        *
 // * specific language governing permissions and limitations under the License.                                              *
 // ***************************************************************************************************************************
-// XML namespaces used in this package
 
 /**
- * AddressBookResource Example
+ * Predefined Transforms
  */
-@XmlSchema(
-	prefix="ab",
-	xmlNs={
-		@XmlNs(prefix="ab", namespaceURI="http://www.apache.org/addressBook/"),
-		@XmlNs(prefix="per", namespaceURI="http://www.apache.org/person/"),
-		@XmlNs(prefix="addr", namespaceURI="http://www.apache.org/address/"),
-		@XmlNs(prefix="mail", namespaceURI="http://www.apache.org/mail/")
-	}
-)
-@RdfSchema(
-	prefix="ab",
-	rdfNs={
-		@RdfNs(prefix="ab", namespaceURI="http://www.apache.org/addressBook/"),
-		@RdfNs(prefix="per", namespaceURI="http://www.apache.org/person/"),
-		@RdfNs(prefix="addr", namespaceURI="http://www.apache.org/address/"),
-		@RdfNs(prefix="mail", namespaceURI="http://www.apache.org/mail/")
-	}
-)
-package org.apache.juneau.examples.addressbook;
-import org.apache.juneau.jena.annotation.*;
-import org.apache.juneau.xml.annotation.*;
+package org.apache.juneau.transforms;
 
