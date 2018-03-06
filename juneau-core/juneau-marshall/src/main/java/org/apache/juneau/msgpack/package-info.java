d3aa04219ca16df49665c3e99d6799053d8874bd
JamesBognar <jamesbognar@apache.org>
2018-03-05 21:44:56 -0500

    Update Javadocs.

--- juneau-core/juneau-marshall/src/main/java/org/apache/juneau/msgpack/package-info.java
+++ juneau-core/juneau-marshall/src/main/java/org/apache/juneau/msgpack/package-info.java
@@ -10,66 +10,9 @@
 // * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the        *
 // * specific language governing permissions and limitations under the License.                                              *
 // ***************************************************************************************************************************
-package org.apache.juneau.json.annotation;
-
-import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.RetentionPolicy.*;
-
-import java.lang.annotation.*;
 
 /**
- * Annotation for specifying various JSON options for the JSON serializers and parsers.
- * 
- * <p>
- * Can be applied to Java types.
- * 
- * <p>
- * Can be used for the following:
- * <ul class='spaced-list'>
- * 	<li>
- * 		Wrap bean instances inside wrapper object (e.g. <code>{'wrapperAttr':bean}</code>).
- * </ul>
+ * MessagePack Marshalling Support
  */
-@Documented
-@Target({TYPE})
-@Retention(RUNTIME)
-@Inherited
-public @interface Json {
+package org.apache.juneau.msgpack;
 
-	/**
-	 * Wraps beans in a JSON object with the specified attribute name.
-	 * 
-	 * <p>
-	 * Applies only to {@link ElementType#TYPE}.
-	 * 
-	 * <p>
-	 * This annotation can be applied to beans as well as other objects serialized to other types (e.g. strings).
-	 * 
-	 * <h5 class='figure'>Example:</h5>
-	 * <p class='bcode'>
-	 * 	<ja>@Json</ja>(wrapperAttr=<js>"myWrapper"</js>)
-	 * 	<jk>public class</jk> MyBean {
-	 * 		<jk>public int</jk> f1 = 123;
-	 * 	}
-	 * </p>
-	 * 
-	 * <p>
-	 * Without the <ja>@Json</ja> annotations, serializing this bean as JSON would have produced the following...
-	 * <p class='bcode'>
-	 * 	{
-	 * 		f1: 123
-	 * 	}
-	 * </p>
-	 * 
-	 * <p>
-	 * With the annotations, serializing this bean as JSON produces the following...
-	 * <p class='bcode'>
-	 * 	{
-	 * 		myWrapper: {
-	 * 			f1: 123
-	 * 		}
-	 * 	}
-	 * </p>
-	 */
-	String wrapperAttr() default "";
-}
