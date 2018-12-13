2e2c94e4f11bc8d2be2f233c5484d73597675b6a
Akayeshmantha <kaperera@mitrai.com>
2018-12-13 23:26:14 +0530

    adding uon examples

---
+++ juneau-examples/juneau-examples-core/src/main/java/org/apache/juneau/examples/core/uon/UONExample.java
@@ -0,0 +1,60 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.juneau.examples.core.uon;
+
+import org.apache.juneau.examples.core.pojo.Pojo;
+import org.apache.juneau.parser.ParseException;
+import org.apache.juneau.serializer.SerializeException;
+import org.apache.juneau.uon.UonParser;
+import org.apache.juneau.uon.UonSerializer;
+
+/**
+ *	Sample class which shows the simple usage of UONSerializer.
+ */
+public class UONExample {
+    /**
+     * Serializing SimplePojo bean into UON type
+     * and Deserialize back to Pojo instance type.
+     * @param args
+     * @throws SerializeException
+     * @throws ParseException
+     */
+    public static void main(String[] args) throws SerializeException, ParseException {
+
+        // Fill some data to a Pojo bean
+        Pojo pojo = new Pojo("id","name");
+
+        /**
+         * Produces
+         * (name=name,id=id)
+         */
+        String serial = UonSerializer.DEFAULT.serialize(pojo);
+        System.out.println(serial);
+
+        // Deserialize back to Pojo instance
+        Pojo obj = UonParser.DEFAULT.parse(serial, Pojo.class);
+
+        assert obj.getId().equals(pojo.getId());
+        assert obj.getName().equals(pojo.getName());
+
+        // The object above can be parsed thanks to the @BeanConstructor annotation on PojoComplex
+        // Using this approach, you can keep your POJOs immutable, and still serialize and deserialize them.
+
+    }
+}
