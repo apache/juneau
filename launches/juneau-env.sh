8357803d1c23808df2894481133b60ad95c9b40f
JamesBognar <james.bognar@salesforce.com>
2020-10-01 14:34:01 -0400

    Change release to 8.2.0.

--- launches/juneau-env.sh
+++ launches/juneau-env.sh
@@ -13,6 +13,6 @@
 
 . ~/.profile
 set -e
-export JUNEAU_VERSION=8.1.4
+export JUNEAU_VERSION=8.2.0
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_162.jdk/Contents/Home
 [ -z "$X_VERSION" ] && export X_VERSION="${JUNEAU_VERSION}-SNAPSHOT"
